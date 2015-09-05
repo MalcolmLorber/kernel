96c3a99083990cd477a81d384bccc309cbfe6673
malcolm <lorbem@rpi.edu>
2015-09-05 17:48:10 -0400

    Added multi-stage bootloader that loads real bootloader from disk

--- boot.asm
+++ boot.asm
@@ -1,41 +1,76 @@
-;--------------- PRELIMINARY SETUP ---------------;
-	[BITS 16]	; 16 bit real mode
-	[ORG 0x7C00]	; MBR is loaded at 0x7C00 
+	[bits 16]	; 16 bit real mode
+	[org 0x7C00]	; MBR is loaded at 0x7C00
+
+	jmp 0x0000:st 		;cs=0x0000, for consistency
+st:
+	xor 	ax, ax		;clearing registers
+	mov	ds, ax
+	mov	es, ax
+
+	mov	dl, 0x80	; HDD1
+	xor	dh, dh		; force 0
+	mov	bx, 0x7e00	; read to 7e00, after bootloader
+	mov	al, 1		; num of sectors to read
+	mov	cx, 0x0002	; floppy cylinder
+	call 	read_sectors_16	; read with above args
+	jnc	0x7e00		; jump to actual code
+halt:				
+	cli			; halt if failed, loop
+	hlt
+	jmp halt
+	
+read_sectors_16:
+	pusha
+	mov si, 0x02    ; maximum attempts - 1
+.top:
+	mov ah, 0x02    ; read sectors into memory (int 0x13, ah = 0x02)
+	int 0x13
+	jnc .end        ; exit if read succeeded
+	dec si          ; decrement remaining attempts
+	jc  .end        ; exit if maximum attempts exceeded
+	xor ah, ah      ; reset disk system (int 0x13, ah = 0x00)
+	int 0x13
+	jnc .top        ; retry if reset succeeded, otherwise exit
+.end:
+	popa
+	retn
+	
+read_fail db 'Failure to read from disk.', 13, 10, 0
+	; $ is current line, $$ is first line, db 0 is a 00000000 byte
+	; So, pad the code with 0s until you reach 510 bytes
+times 510 - ($ - $$) db 0
+; Fill last two bytes (a word) with the MBR signature 0xAA55
+	dw 0xaa55
 ;---------------- BOOTLOADER CODE ----------------;
-ls:	MOV SI, str1	; pointer to string in SI
-	CALL PrintStr	; print string
-	;MOV ebx, 1000		;start at 10000
-	;SUB ebx, 1		;down 1 each time
-	;NOP			;waste time
-	;JZ 6			;for loop
-	;JMP ls			; 
-	HLT
+ls:	mov si, str1	; pointer to string in si
+	call PrintStr	; print string
+	jmp Main			; 
+	hlt
 ;---------------- SCREEN FUNCTIONS ---------------;
 PrintStr:	; print string at SI
-nextchar:
-	MOV AL, [SI]	; Grab next char
-	OR AL, AL	; if char is null term
-	JZ exit		; then return
-	CALL PrintChar	; else print char
-	INC SI		; inc pointer to next char
-	JMP nextchar	; loop
+nextChar:
+	mov al, [si]	; grab next char
+	or al, al	; if char is null term
+	jz exit		; then return
+	call PrintChar	; else print char
+	inc si		; inc pointer to next char
+	jmp nextChar	; loop
 	exit:
-	RET
+	ret
 
 PrintChar:	; print char at AL
-	MOV AH, 0x0E	; Teletype Mode
-	MOV BH, 0x00	; Page zero
-	MOV BL, 0x07	; Light Gray
-	INT 0x10	; Print Character
-	RET
+	mov ah, 0x0e	; teletype mode
+	mov bh, 0x00	; page zero
+	mov bl, 0x07	; light gray
+	int 0x10	; print character
+	ret
 ;------------------ DATA BLOCK ------------------;
 
 str1 db 'Hello World', 0		;stuff
-;-------------- PADDING / SIGNATURE -------------;
-	; $ is current line, $$ is first line, db 0 is a 00000000 byte
-	; So, pad the code with 0s until you reach 510 bytes
-TIMES 510 - ($ - $$) DB 0
-; Fill last two bytes (a word) with the MBR signature 0xAA55
-	DW 0xAA55
+str2 db 'WHY IS THIS', 0
 
 Main:	;main section
+	nop
+	nop
+	mov si, str2
+	call PrintStr

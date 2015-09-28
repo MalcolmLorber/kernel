f9adcd738a433eced24f43d255d4a13d551c1b22
adam <adam@triazo.net>
2015-09-28 01:16:49 -0400

    Add assembly library to enable paging

--- src/mem.h
+++ src/mem.h
@@ -5,6 +5,10 @@
 #pragma once
 #include <stdint.h>
 
+// These two are defined in the assembly file paging.asm
+extern void loadPageDirectory(uint32_t*);
+extern void enablePaging();
+
 // Defined in the file mem.c
 uint32_t* initiate_directory();
  void initiate_page_table(uint32_t* page_dir);

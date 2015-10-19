813958ac891556a907331a41fbe5ac1a68b6f8e4
malcolm <lorbem@rpi.edu>
2015-10-19 11:38:47 -0400

    Fixed order of regs in reg_state

--- src/idt.h
+++ src/idt.h
@@ -30,10 +30,10 @@ struct reg_state
     uint32_t ecx;
     uint32_t edx;
     uint32_t ebx;
+    uint32_t esp;
     uint32_t ebp;
     uint32_t esi;
     uint32_t edi;
-    uint32_t esp;
 }__attribute__((packed));
 
 struct stack_state

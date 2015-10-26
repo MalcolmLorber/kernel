246a4b2b16f980d42f9f724673fb1f810dafff87
malcolm <lorbem@rpi.edu>
2015-10-26 11:03:27 -0400

    Removed reliance on serial_hexword

--- src/io.c
+++ src/io.c
@@ -2,10 +2,16 @@
 #include "pic.h"
 #include "serial.h"
 #include "idt.h"
+#include "string.h"
+
+#define serial_val(i) itoa(i,f);serial_writestring(f);serial_writestring(" ");
 
 void kbd_irq()
 {
-    serial_hexword(inb(0x60));
+    char f[20];
+    //serial_hexword(inb(0x60));
+    serial_val(inb(0x60));
+    serial_writestring("\n");
     pic_command(0, PIC_OCW2_EOI);
 }
 

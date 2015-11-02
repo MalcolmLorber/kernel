8be723c863d43b2b914a2b51e11ded4d1ab4f748
malcolm <lorbem@rpi.edu>
2015-11-02 10:28:41 -0500

    Changed mapping for more control characters

--- src/io.c
+++ src/io.c
@@ -2,11 +2,19 @@
 #include "pic.h"
 #include "serial.h"
 #include "idt.h"
-#include "string.h"
+
+char scan_map[256] = {' ','E','1','2','3','4','5','6','7','8','9','0','-','=','\b','\t','q','w','e','r','t','y','u','i','o','p',
+		      '[',']','\n','^','a','s','d','f','g','h','j','k','l',';','\'','`','S','\\','z','x','c','v','b','n','m',
+		      ',','.','/','S','O','A',' ','C','F','F','F','F','F','F','F','F','F','F','L','L',};
 
 void kbd_irq()
 {
-    serial_hexword(inb(0x60));
+    int a = inb(0x60);
+    serial_hexword(a);
+    if(a<0x80)
+	terminal_putchar(scan_map[a]);
+    //else
+    //terminal_putchar(scan_map[a-0x80]);
     serial_writestring("\n");
     pic_command(0, PIC_OCW2_EOI);
 }

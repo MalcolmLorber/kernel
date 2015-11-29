188429fd6156c036e8023f2d59483e8d4d09954b
malcolm <lorbem@rpi.edu>
2015-11-29 09:12:06 -0500

    mutex create function and threadid

--- src/mutex.c
+++ src/mutex.c
@@ -1,5 +1,14 @@
 #include "mutex.h"
 
+mutex mutex_create()
+{
+    mutex m;
+    m.lock_var=0;
+    m.value=0;
+    m.thread_id=0;
+    return m;
+}
+
 uint32_t mutex_lock(mutex m)
 {
     asm volatile
--- src/mutex.h
+++ src/mutex.h
@@ -7,8 +7,10 @@ typedef struct
 {
     uint32_t lock_var;
     uint32_t value;
+    uint32_t thread_id;
 }__attribute__((packed)) mutex;
 
+mutex mutex_create();
 uint32_t mutex_lock(mutex m);
 uint32_t mutex_unlock(mutex m);
 
